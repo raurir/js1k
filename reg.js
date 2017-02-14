10802c3fd1a5e492933d1f289184572d33bd4d50
raurir <raurir@gmail.com>
2017-02-15 09:35:55 +1100

    yin yang'd the yang yin into yin, yang, yin-yang & yang-yin

--- reg.js
+++ reg.js
@@ -1,15 +1,17 @@
+const input = process.argv[2], output = process.argv[3];
+console.log("reg", input, output);
+
 "use strict";
 
 var fs = require('fs');
 var regPack = require("./regpackrauri/regPack.js");
 var cmdRegPack = regPack.cmdRegPack;
-var input = fs.readFileSync("magic.js", 'utf-8');
-var result = cmdRegPack(input, {
+var inputFile = fs.readFileSync(`${input}.js`, 'utf-8');
+var result = cmdRegPack(inputFile, {
 	hash2DContext: true,
 	crushGainFactor: 1,
 	crushLengthFactor: 0,
 	crushCopiesFactor: true
 });
 // console.log(result);
-fs.writeFile("magic-compresses-regpack.js", result)
-// cmdRegPack()
+fs.writeFile(`${output}.js`, result)
