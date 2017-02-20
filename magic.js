e78740c020b2f96244d0e146140330b9afb8ed63
Rauri Rochford <raurir@gmail.com>
2017-02-20 23:41:11 +1100

    up down blah

--- magic.js
+++ magic.js
@@ -4,20 +4,22 @@ e = d.getContext("2d");
 
 // debug = document.createElement("div");
 // b.appendChild(debug)
-// b.appendChild(d);
+b.appendChild(d);
 
 var size = a.width = a.height = 1024, 
 	M = Math, 
 	sc = 1, 
 	scaleTarget = 1, 
 	beginWarp = 1, // true 
-	third = M.PI / 3,
+	third = M.PI / 3, 
 	parts = [], 
 	dead = [];
 
 d.width = d.height = size * 2;
 
 var r = v => ~~(M.random() * v);
+
+var sixit = v => v;//(v + 6) % 6; // clamp v between 0 and 6
 
 var create = (parent, d, mod) => {
 	var p;
@@ -34,32 +36,47 @@ var create = (parent, d, mod) => {
 	
 	if (parent && dead.length) {
 		p = dead.splice(-1)[0];
+		p.d = d;
+		p.x = p.lx = x;
+		p.y = p.ly = y;
 		p.o = 0; // false;
-		p.d = d;		
+		p.pos = 0;
+		p.dying = r(4) + 1;
+		p.alive = 1; // true
 		p.colour = parent.colour;
+
+		//p.setColour();
 		
 	} else {
 
-		if (parts.length > size) return ;//con.warn("too many!");
+		// if (parts.length > size) return ;//con.warn("too many!");
 
 		p = {
 
-			r: () => { // reset
-				p.alive = 1; // true
-				p.x = p.lx = x;
-				p.y = p.ly = y;
-				p.dying = r(4) + 1;
-				p.pos = 0;
-			},
+			pos: 0,
+			dying: 1,
+			alive: 1, // true
+			x,
+			y,
+			lx: x,
+			ly: y,
 
 			o,
+			life: 0,
 			d: d || r(3) * 2, // 0, 2 or 4
 			mod,
-			s: 2 * mod,
+			s: 8 * mod,
+			// s: 1 / M.pow(2, r(2)),
+			// setColour: () => {
+				// p.colour = "hsla(" + (p.life++ * 20) + ",50%," + (20 + p.s * 20) +"%,0.7)";
 			colour: parent ? parent.colour : "hsla(" + (150 + parts.length * 20) + ",50%,60%,.8)",
-			// p.colour = parent ? parent.colour : "hsla(" + r(360) + ",50%,50%,1)";
+				// p.colour = parent ? parent.colour : "hsla(" + r(360) + ",50%,50%,1)";
+				// con.log(p.colour)
+			// },
 			kill: () => {
-				p.alive = 0; // false
+				// p.x = 0;
+				// p.y = 0;
+				p.alive = 0; //false;
 				dead.push(p);
 			},
 			m: () => {
@@ -77,10 +94,10 @@ var create = (parent, d, mod) => {
 					return;
 				}
 
-				p.pos += 1 / 32; // p01 thanks! using 1 / (powers of 2)
-				if (p.pos == 1) {
+				p.pos += 1 / 8; //p01!!
+				if (p.pos==1) {
 
-					if (p.x < 0 || p.y > size * 2 || p.y < 0 || p.y > size * 2) {
+					if (p.x < 0 || p.x > size * 2 || p.y < 0 || p.y > size * 2) {
 						if (p.o) {
 							create();
 						} else {
@@ -89,19 +106,26 @@ var create = (parent, d, mod) => {
 						return;
 					}
 
-					p.pos = 0;
-					p.d += r(2) * 2 - 1; // add -1 or 1 to new dir.
 
-					if (p.mod > 1 / 4 && r(9) > 2) { // duplicate at current position
+					p.pos = 0;
+					// p.setColour();
+					p.d += r(2) * 2 - 1; // add -1 or 1 to new dir.
+					// p.d += r(16);//
+
+					if (p.mod > 1 / 4 && r(9) > 7) { // duplicate at current position
 						
 						// var newDir = r(2) * 4 - 2; // + -2 or 2
 						// newDir = p.d + newDir; // make sure clone has new direction
 						// newDir = (newDir + 6) % 6; // clamp to positives: 0 > 5
 						for (var i = r(6); i--;) {
 							// con.log(i)
+							
 							var newDir = p.d + r(2) * 4 - 2; // same as above 3 lines
+							
 							// e.fillStyle = "red"
 							// e.fillRect(p.x - 10, p.y - 10, 20, 20);//p.s, p.s);
+
+							// create(false, p.x, p.y, newDir, p.mod / 2);
 							create(p, newDir, p.mod / 2);
 							// debugger;
 						}
@@ -118,6 +142,7 @@ var create = (parent, d, mod) => {
 					// e.fillRect(p.x, p.y, 1, 1);//p.s, p.s);
 					// e.fillText(p.index, p.x, p.y + 10);
 					e.lineWidth = p.mod * 6;
+					// e.strokeStyle = p.o ? "#fff" : p.colour;
 					e.strokeStyle = p.colour;
 					e.beginPath();
 					e.moveTo(p.lx, p.ly);
@@ -127,17 +152,20 @@ var create = (parent, d, mod) => {
 					p.ly = p.y;
 
 				}
+				// if (p.d < 0 || p.d > 5) {con.log("aargh", p.d);}
+
+				// if (M.round(p.x * 32) != p.x * 32) con.log("unround", p.x)
+
 
 			}
 		}
 
+		// p.setColour();
 		// p.index = parts.length;
 		parts.push(p);
 
+
 	}
-
-	p.r();
-
 
 };
 var render = (t) =>{
@@ -145,7 +173,7 @@ var render = (t) =>{
 
 	// debug.innerHTML = parts.map(p=>Math.round(p.x)); 
 
-	if ((0|t / size) % 5 == 0) { // Math.round
+	if ((0|(t / size)) % 5 == 0) {
 		if (!beginWarp) { // warp has just begun! fuck yeah.
 			beginWarp = 1; // true
 			scaleTarget = M.random() + .7; // Math.sqrt(2) / 2 is min scale
@@ -159,7 +187,7 @@ var render = (t) =>{
 	c.save();
 	c.translate(size / 2, size / 2);
 	c.scale(sc, sc);
-	c.rotate(t * .0001); // arbitrary divisor
+	c.rotate(t * 0.0001); // arbitrary divisor
 	c.translate(-size, -size);
 
 	e.fillStyle = "rgba(0,0,0,.04)";
